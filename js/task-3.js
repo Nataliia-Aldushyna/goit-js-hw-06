c589c32bfdcf2acb40b70d7cb063ae2976b17a58
Nataliia Aldushyna <nataliia.aldushyna@gmail.com>
2024-09-09 09:44:00 +0200

    update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -3,29 +3,27 @@ class StringBuilder {
   constructor(value) {
     this.#value = value;
   }
+
   getValue() {
     return this.#value;
   }
-
   padEnd(str) {
-    this.#value = `${this.#value}${str}`
+    this.#value += str;
   }
-
   padStart(str) {
-    this.#value = `${str}${this.#value}`
+    this.#value = str + this.#value;
   }
-
   padBoth(str) {
-    this.#value = `${str}${this.#value}${str}`
+    this.padStart(str);
+    this.padEnd(str);
   }
-
 }
 
-const builder = new StringBuilder(".");
+const builder = new StringBuilder('.');
 console.log(builder.getValue()); // "."
-builder.padStart("^");
+builder.padStart('^');
 console.log(builder.getValue()); // "^."
-builder.padEnd("^");
+builder.padEnd('^');
 console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
+builder.padBoth('=');
 console.log(builder.getValue()); // "=^.^="
